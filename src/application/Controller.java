8ef35067732828d2d55e34b68891317b51363784
Henry <Henry@LAPTOP-D4VKIG9B>
2020-03-03 23:10:44 +0000

    controller

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,12 +1,53 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
 
     private Client client;
-//todo work out host for client argument
-    public Controller() {
-        this.client = new Client("localhost");
-    }
+    private Main main;
+    @FXML
+	private TextField usernameField;
+	@FXML
+	private PasswordField passwordField;
+	@FXML
+	private Button LogInButton;
 
+	
+	//todo work out host for client argument
+    public Controller() {
+//        this.client = new Client("localhost");
+    }
+    
+    @FXML
+	protected void login(MouseEvent event) throws IOException {
+		if (!usernameField.getText().isEmpty() && !passwordField.getText().isEmpty()) {
+			String loginToServer = "login " + usernameField.getText() + " "+ passwordField.getText();
+			this.client.login(loginToServer);
+		} else {
+			System.out.println("login failed");
+
+		}
+	}
+    
+    
+    public Main getMain() {
+		return main;
+	}
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
 
 }
