f9626f94ddf08844e4eb147f2ee0bc8f19430a2b
Henry <Henry@LAPTOP-D4VKIG9B>
2020-03-03 23:11:09 +0000

    main update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,56 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	Stage primaryStage;
+	Client client;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
-        primaryStage.setTitle("BattleShips");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+//        primaryStage.setTitle("BattleShips");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+    	client = new Client("localhost");
+    	setLoginStage(primaryStage);
+    	
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+    
+	public void setLoginStage(Stage primaryStage) throws IOException {
+		 FXMLLoader loader = new FXMLLoader();
+	        URL url = new File("res/loginFX.fxml").toURI().toURL();
+	        loader.setLocation(url);
+	        VBox vbox = loader.<VBox>load();
+	        Scene scene = new Scene(vbox);
+	        primaryStage.setScene(scene);
+	        primaryStage.getIcons().add(new Image("icon.png"));
+	        
+		Controller controller = loader.getController();
+		controller.setClient(client);
+		controller.setMain(this);// pointing to this instance
+		this.client.setController(controller);
+		primaryStage.setResizable(false);
+		primaryStage.sizeToScene();
+		primaryStage.setTitle("Battleships Asos");
+
+
+	        primaryStage.show(); 
+	}
 }
