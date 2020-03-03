12f0aee0d95f0946ab5ab0cc5d6260fded9888ee
ELIZABETH Atkins <eka904@cs.bham.ac.uk>
2020-03-03 18:03:43 +0000

    Update Board.java now possible to change ship length by pressing up/down arrows

--- src/GUI/Board.java
+++ src/GUI/Board.java
@@ -1,174 +1,140 @@
-import java.util.ArrayList;
-
+import javafx.application.*;
 import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.*;
+import javafx.scene.input.*;
+import javafx.scene.text.Text;
 
-public class Board {
-	private boolean isEnemy;
-	private ArrayList<Cell> cells;
-	private int ships;
-	private boolean isVertical = true;
-	private int shipLength;
-	Board(boolean enemy,  EventHandler<? super MouseEvent> handler){
-		cells = new ArrayList<Cell>();
-		isEnemy = enemy;
-		for(int i = 0; i<10;i++) {
-			for(int j = 0; j < 10; j++) {
-				Cell c = new Cell(i*50,j*50);
-				cells.add(c);
-				 c.setOnMouseClicked(handler);
+import java.util.Random;
+/**
+ * Main class for the GUI
+ * @author ELizabeth Atkins
+ * Improvements that need to be made:
+ * Ships should not be able to overlap
+ * */
+public class GUIMain extends Application{
+	private static Board playerBoard;
+	private static Board enemyBoard;
+	private static boolean playerTurn;
+	public static void main(String[] args) {
+		setUp();
+		launch(args);
+	}
+	public static void setUp() {
+		//enables the player to place ships
+		playerTurn = true;
+		 playerBoard = new Board(false, event -> {
+	           Board.Cell cell = (Board.Cell) event.getSource();
+	           //playerBoard.setShipLength(4);
+	           playerBoard.placeShip(playerBoard.getCells().indexOf(cell));
+	           });
+		//enables the player to strike the enemy board
+		enemyBoard = new Board(true, event ->  {
+				Board.Cell cell = (Board.Cell) event.getSource();
+				if(playerBoard.getShips() == 5) {
+				cell.strike();
+				if(cell.isHit()) {
+					playerTurn = false;
+				}
+				if(!playerTurn) {
+	   				simulateEnemy();
+	   			}
+				}
+			});
+			//Having "enemy" place random ships for testing purposes
+			Random rand = new Random();
+			enemyBoard.setShipLength(4);
+			for(int i = 0; i < 5;i++) {
+				enemyBoard.placeShip(rand.nextInt(100));
+			}
+	}
+	/**
+	 * Method to simulate an enemy for testing purposes
+	 * */
+	private static void simulateEnemy() {
+		while(!playerTurn) {
+			Random r = new Random();
+			Board.Cell cell = playerBoard.getCell(r.nextInt(100));
+			cell.strike();
+			if(cell.isHit()) {
+				playerTurn = true;
 			}
 		}
 	}
-	public ArrayList<Cell> getCells(){
-		return cells;
-	}
-	public int getShips() {
-		return ships;
-	}
-	public Cell getCell(int i) {
-		return cells.get(i);
-	}
-	public void placeShip(int i, int length) {
-		shipLength = length;
-		Cell cell = getCell(i);
-		if(isEnemy == false && ships < 5 && isVertical == false) {
-			cell.isShip = true;
-			cell.setFill(Color.WHITE);
-			int n = 1;
-			if(i < (100 - length*10)) {
-				while(n < length){
-					getCell(i+10).setFill(Color.WHITE);
-					getCell(i+10).isShip = true;
-					i+=10;
-					n++;
-				}
-			}else {
-				while(n < length) {
-					getCell(i-10).setFill(Color.WHITE);
-					getCell(i-10).isShip = true;
-					i-=10;
-					n++;
-				}
-			}
-		ships++;
-		}else if(isEnemy == true && ships < 5 && isVertical == false){
-			cell.isShip = true;
-			//cell.setFill(Color.GREEN);
-			int n = 1;
-			if(i < (100 - length*10)) {
-				while(n < length){
-					//getCell(i+10).setFill(Color.GREEN);
-					getCell(i+10).isShip = true;
-					i+=10;
-					n++;
-				}
-			}else {
-				while(n < length) {
-					//getCell(i-10).setFill(Color.GREEN);
-					getCell(i-10).isShip = true;
-					i-=10;
-					n++;
-				}
-			}
-			ships++;
-			
+	/**
+	 * The enemy board and the player board come up in two different windows that annoyingly load
+	 * on top of each other
+	 * */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Group group = new Group();
+		for(Board.Cell c : playerBoard.getCells()) {
+			group.getChildren().add(c);
 		}
-		if(isEnemy == false && ships < 5 && isVertical == true) {
-			boolean overEdge = false;
-			int n = 1;
-			cell.isShip = true;
-			cell.setFill(Color.WHITE);
-			for(int j = i; j < (i + length); j++) {
-				if(j % 10 == 0 && j!= i) {
-					overEdge = true;
-					j = i + length;
-				}else {
-					overEdge = false;
-				}
-			}
-			if(overEdge == false) {
-				while(n<length) {
-				getCell(i+1).isShip = true;
-				getCell(i+1).setFill(Color.WHITE);
-				i++;
-				n++;
-				}
-			}else {
-				while(n<length) {
-					getCell(i-1).isShip = true;
-					getCell(i-1).setFill(Color.WHITE);
-					i--;
-					n++;
-				}
-			}
-			ships++;
-		}else if(isEnemy == true && ships < 5 && isVertical == true) {
-			boolean overEdge = false;
-			int n = 1;
-			for(int j = i; j < (i + length); j++) {
-				if(j % 10 == 0 && j!= i) {
-					overEdge = true;
-					j = i + length;
-				}else {
-					overEdge = false;
-				}
-			}
-			if(overEdge == false) {
-				while(n<length) {
-				getCell(i+1).isShip = true;
-				i++;
-				n++;
-				}
-			}else {
-				while(n<length) {
-					getCell(i-1).isShip = true;
-					i--;
-					n++;
-				}
-			}
-		}
-	}
-	public int getShipLength() {
-		return shipLength;
-	}
-	public void setShipLength(int shipLength) {
-		this.shipLength = shipLength;
-	}
-	public class Cell extends Rectangle{
-		private boolean hit;
-		private boolean isShip;
-		Cell(int x,int y){
-			super(50,50);
-			this.setFill(Color.LIGHTSKYBLUE);
-			this.setStroke(Color.BLACK);
-			hit = false;
-			isShip = false;
-			this.setX(x);
-			this.setY(y);
-		}
-		public void strike() {
-			if(isShip == false) {
-				hit = true;
-				this.setFill(Color.BLACK);
-			}else if(isShip == true){
-				hit = true;
-				this.setFill(Color.ORANGE);
-			}else {
+		Text text = new Text("Press H to set ships horizontal, press V to set ships vertical. \n" +
+		"Press up arrow to increase ship size, press down arrow to decrease ship size");
+		//Text secondLine = new Text("Ships placed: " + playerBoard.getShips());
+		text.setX(10);
+		text.setY(520);
+		//secondLine.setX(10);
+		//secondLine.setY(530);
+		//group.getChildren().add(secondLine);
+		group.getChildren().add(text);
+		//group.getChildren().add(secondLine);
+		Scene scene = new Scene(group);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Battleship - Player");
+		primaryStage.show();
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent keyEvent) {
+
+	            switch (keyEvent.getCode()) {
+	                case V:
+	                    playerBoard.setVertical();
+	                    break;
+	                case H:
+	                	playerBoard.setHorizontal();
+	                	break;
+	               // case NUMPAD1:
+	               // 	playerBoard.setShipLength(1);
+	               // 	break;
+	               // case NUMPAD2:
+	               // 	playerBoard.setShipLength(2);
+	               // 	break;
+	               // case NUMPAD3:
+	               // 	playerBoard.setShipLength(3);
+	               // 	break;
+	               // case NUMPAD4:
+	               // 	playerBoard.setShipLength(4);
+	               // 	break;
+	               // case NUMPAD5:
+	               // 	playerBoard.setShipLength(5);
+	               // 	break;
+	                case UP:
+	                	playerBoard.setShipLength(playerBoard.getShipLength() + 1);
+	                	break;
+	                case DOWN:
+	                	playerBoard.setShipLength(playerBoard.getShipLength() - 1);
+	                	break;
+	                	
+
+	            }
 				
 			}
+			
+		});
+
+		Stage secondStage = new Stage();
+		Group secondGroup = new Group();
+		for(Board.Cell c : enemyBoard.getCells()) {
+			secondGroup.getChildren().add(c);
 		}
-		public boolean isHit() {
-			return hit;
-		}
-		public int getIndex() {
-			if(cells.contains(this)) {
-				return cells.indexOf(this);
-			}else {
-				throw new IllegalArgumentException();
-			}
-		}
+		Scene scene2 = new Scene(secondGroup);
+		secondStage.setScene(scene2);
+		secondStage.setTitle("Battleship - Enemy");
+		secondStage.show();
 	}
 }
