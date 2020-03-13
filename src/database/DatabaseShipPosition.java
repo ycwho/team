e2a636f72d9ae5d41eb55aaba83fecb3cb863aa4
Ning Wei <nxw935@cs.bham.ac.uk>
2020-03-13 15:42:34 +0000

    change parameter

--- src/database/DatabaseShipPosition.java
+++ src/database/DatabaseShipPosition.java
@@ -1,4 +1,7 @@
+
+
 import java.io.File;
+
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,7 +10,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
-
+import java.util.regex.Matcher;
 import com.sun.org.apache.regexp.internal.recompile;
 
 /**
@@ -37,8 +40,7 @@ public class DatabaseShipPosition {
 
 	/**
 	 * save shipPosition(the player can create a slot table and insert
-	 * shipType,shipPosition in the table) ;
-	 * save the strategic layout to the slots,
+	 * shipType,shipPosition in the table) ; save the strategic layout to the slots,
 	 * each slot is a table in database: the username is the primary key, and each
 	 * ship position takes one row. Before use this method, Server should check
 	 * whether the ShipPosition[] is overlapped first
@@ -48,55 +50,76 @@ public class DatabaseShipPosition {
 	 * @param positions positions of the ships
 	 * @return
 	 */
-	public int saveShipPosition(String userName, String slotName, ShipPosition[] positions) {
-		// int result = 0; the value returned based on server,so it will change after server finished
+	public int saveShipPosition(String shipPositionString) {
+		// int result = 0; the value returned based on server,so it will change after
+		// server finished
 		boolean check = false;
-
+		String[] positions = shipPositionString.split(",");
+		String username = positions[0];
+		String option = positions[1];
+		String position = positions[2];
 		// Check if the player has pre-saved slots
 		try {
-			ResultSet resultTable = connection.getMetaData().getTables(null, null, slotName, null);
+			ResultSet resultTable = connection.getMetaData().getTables(null, null, username, null);
 			if (resultTable.next()) {
 				check = true;
-				System.out.println("The slot has existed!");
+
+				StringBuffer sbInsert = new StringBuffer();
+				sbInsert.append("INSERT INTO " + username);
+				sbInsert.append(" (username,option,shipPositions) " + "VALUES (?,?,?)");
+				String sqlInsert = sbInsert.toString();
+				System.out.println(sqlInsert);
+
+				PreparedStatement insertStatement = connection.prepareStatement(sqlInsert);
+				System.out.println("connect");
+
+				insertStatement.setString(1, username);
+				System.out.println("u");
+				insertStatement.setString(2, option);
+				System.out.println("o");
+				insertStatement.setString(3, position);
+				System.out.println("p");
+				insertStatement.executeUpdate();
+				// result = insertStatement.executeUpdate();
+				System.out.println("Insert successfully");
 			}
 
-			// Create a new slot table according to the warship's information
+			// Create a new table according to the warship's information
 			if (check == false) {
 
 				StringBuffer sb = new StringBuffer();
-				sb.append("CREATE TABLE IF NOT EXISTS " + slotName);
-				sb.append(" (userName CHAR(20) ,shipType int, status int,xPosition int,yPosition int);");
+				sb.append("CREATE TABLE IF NOT EXISTS " + username);
+				sb.append(
+						" (userName varchar(7000) ,option varchar(7000) ,shipPositions varchar(7000),foreign key(username) references users(username));");
 				String str = sb.toString();
 				System.out.println(str);
 
 				Statement stmt = connection.createStatement();
 				stmt.executeUpdate(str);
-				System.out.println("The slot created successfully!");
+				System.out.println("The table created successfully!");
 
-				// insert values into a new slot table.
-				for (int i = 0; i < positions.length; i++) {
-					StringBuffer sbInsert = new StringBuffer();
-					sbInsert.append("INSERT INTO " + slotName);
-					sbInsert.append(" (Username,shipType,status,xPosition,yPosition) " + "VALUES (?,?,?,?,?)");
-					String sqlInsert = sbInsert.toString();
-					System.out.println(sqlInsert);
+				// insert values into a new table.
 
-					PreparedStatement insertStatement = connection.prepareStatement(sqlInsert);
-					System.out.println("connect");
-					insertStatement.setString(1, userName);
-					System.out.println("1");
-					insertStatement.setInt(2, positions[i].getType());
-					insertStatement.setInt(3, positions[i].getStatus());
-					insertStatement.setInt(4, positions[i].getxPosition());
-					insertStatement.setInt(5, positions[i].getyPosition());
-					insertStatement.executeUpdate();
-					// result = insertStatement.executeUpdate();
-					System.out.println("Insert successfully");
-				}
+				StringBuffer sbInsert = new StringBuffer();
+				sbInsert.append("INSERT INTO " + username);
+				sbInsert.append(" (username,option,shipPositions) " + "VALUES (?,?,?)");
+				String sqlInsert = sbInsert.toString();
+				System.out.println(sqlInsert);
+
+				PreparedStatement insertStatement = connection.prepareStatement(sqlInsert);
+				System.out.println("connect");
+				insertStatement.setString(1, username);
+				insertStatement.setString(2, option);
+				insertStatement.setString(3, position);
+				insertStatement.executeUpdate();
+				// result = insertStatement.executeUpdate();
+				System.out.println("Insert successfully");
 			}
 		}
 
-		catch (Exception e) {
+		catch (
+
+		Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
@@ -110,54 +133,58 @@ public class DatabaseShipPosition {
 	 * @param username
 	 * @return ship positions
 	 */
-	public ArrayList<ShipPosition> loadShipPosition(String userName, String slotName) {
-		ArrayList<ShipPosition> shipPositions = new ArrayList<ShipPosition>();
+	public String loadShipPosition(String shipPosition) {
+		String positions = null;
+		String[] loadPosition = shipPosition.split(",");
+
+		String username = loadPosition[0];
+		String option = loadPosition[1];
 		try {
 			// Check if the player has pre-saved slots
-			ResultSet resultTable = connection.getMetaData().getTables(null, null, slotName, null);
+			ResultSet resultTable = connection.getMetaData().getTables(null, null, username, null);
 			if (resultTable.next()) {
 
 				Statement stmt = connection.createStatement();
 				StringBuffer selectSql = new StringBuffer();
-				selectSql.append("SELECT * FROM ");
-				selectSql.append(slotName);
+				selectSql.append("SELECT positions FROM ");
+				selectSql.append(username + "where option= ");
+				selectSql.append(option + ";");
 				String selectString = selectSql.toString();
 
 				ResultSet rs = stmt.executeQuery(selectString);
-
 				while (rs.next()) {
-					int type = rs.getInt("shipType");
-					int status = rs.getInt("status");
-					int x = rs.getInt("xPosition");
-					int y = rs.getInt("yPosition");
-					ShipPosition position = new ShipPosition(type, status, x, y);
-					shipPositions.add(position);
+					positions = rs.getString("shipPositions");
 				}
-
 			}
-		} catch (Exception e) {
+		} catch (
+
+		Exception e) {
 			e.printStackTrace();
 		}
-		return shipPositions;
+		return positions;
 	}
 
-/**
- * 
- * @param slotName
- * @return true if the player delete the slot successfully.
- */
-	public boolean dropSlot(String slotName) {
+	/**
+	 * 
+	 * @param slotName
+	 * @return true if the player delete the slot successfully.
+	 */
+	public boolean deleteOption(String optionServer) {
 		boolean result = true;
+		String[] deleteOption = optionServer.split(",");
+		String username = deleteOption[0];
+		String option = deleteOption[1];
 		try {
 			StringBuffer sbDelete = new StringBuffer();
-			sbDelete.append("drop table " + slotName);
+			sbDelete.append("delete from " + username);
+			sbDelete.append("where option = " + option + ";");
 			String sqlDelete = sbDelete.toString();
 
 			Statement st = connection.createStatement();
 			st.executeUpdate(sqlDelete);
 
 			StringBuffer sbSelect = new StringBuffer();
-			sbSelect.append("select * from " + slotName);
+			sbSelect.append("select * from " + username + ";");
 			String sqlSelect = sbSelect.toString();
 
 			ResultSet rs = st.executeQuery(sqlSelect);
