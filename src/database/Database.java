1ddeb47a9b07b46b44629e2f3a520d60ba9b2065
Ning Wei <nxw935@cs.bham.ac.uk>
2020-03-18 23:58:53 +0000

    add record score method

--- src/database/Database.java
+++ src/database/Database.java
@@ -29,8 +29,15 @@ public class Database {
 		}
 	}
 
-	// Checking if the user' information existed(if the user has registered?)?
-	// if not, return false ,and insert his username,password.
+	/**
+	 * 
+	 * Checking whether the user's information existed(if the user has registered?)?
+	 * if not, return false ,and insert his user name,password.
+	 * 
+	 * @param user
+	 * @param pass
+	 * @return true the user' data existed
+	 */
 	public boolean checkExistUser(String user, String pass) {
 		boolean check = false;
 		try {
@@ -39,6 +46,7 @@ public class Database {
 			ResultSet resultSet = selectStatement.executeQuery();
 			while (resultSet.next()) {
 				String username = resultSet.getString("username");
+
 				if (user.equals(username)) {
 					check = true;
 					break;
@@ -50,8 +58,14 @@ public class Database {
 		return check;
 	}
 
-	// Checking if the user' information existed(if the user has registered?)?
-	// if not, return false ,and insert his username,password.
+	/**
+	 * 
+	 * Checking whether the user's password right? if not, return false .
+	 * 
+	 * @param user
+	 * @param pass
+	 * @return true the password is right
+	 */
 	public boolean checkPassword(String user, String pass) {
 		boolean check = false;
 		try {
@@ -72,7 +86,12 @@ public class Database {
 		return check;
 	}
 
-	// Insert new user' information who has not registered before.
+	/**
+	 * Insert new user' information who has not registered before.
+	 * 
+	 * @param user
+	 * @param pass
+	 */
 	public void insertUser(String user, String pass) {
 		try {
 			String sql = "INSERT INTO USERS (Username,Password) " + "VALUES (?,?)";
@@ -89,44 +108,13 @@ public class Database {
 
 	}
 
-	public static void main(String[] args) throws IOException {
-		Database d = new Database();
-
-		try (Connection connection = DriverManager.getConnection(d.url, d.username, d.password);
-				PreparedStatement insertStatement = connection
-						.prepareStatement("INSERT INTO USERS (Username,Password) " + "VALUES (?,?) ");
-				PreparedStatement selectStatement = connection
-						.prepareStatement("SELECT Username, Password FROM USERS WHERE " + "Username= ?")) {
-
-			System.out.println("Connection established");
-
-			insertStatement.setString(1, "Tom2");
-			insertStatement.setString(2, "Password");
-
-			// execute the statement
-			insertStatement.executeUpdate();
-
-			// more such data
-			insertStatement.setString(1, "Jim2");
-			insertStatement.setString(2, "Pass");
-
-			insertStatement.executeUpdate();
-
-			selectStatement.setString(1, "John");
-			try (ResultSet resultSet = selectStatement.executeQuery()) {
-				while (resultSet.next()) {
-					String username = resultSet.getString("Username");
-					String password = resultSet.getString("Password");
-
-					System.out.println(username + " " + password);
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			// System.out.println("SQLException");
-		}
-	}
-
+	/**
+	 * 
+	 * @param username
+	 * @param option
+	 * @param position
+	 * @return if database insert successfully,return 0
+	 */
 	public int saveShipPosition(String username, String option, String position) {
 		int result = 1;// if database insert successfully,return 0.
 		boolean check = false;
@@ -230,7 +218,7 @@ public class Database {
 			Statement st = connection.createStatement();
 			st.executeUpdate(sqlDelete);
 
-			StringBuffer select=new StringBuffer();
+			StringBuffer select = new StringBuffer();
 			select.append("select * from shipposition where option ='");
 			select.append(option + "' and username ='" + username + "';");
 			String sqlSelect = sbDelete.toString();
@@ -246,4 +234,104 @@ public class Database {
 		}
 		return result;
 	}
+
+	/**
+	 * record score,if three people play the game, the winner get 2 and the other two loser get -1 respectively.
+	 * @param user
+	 * @param score
+	 * @return 0 if record score successfully.
+	 */
+	public int recordScore(String playerName, int score) {
+		boolean check = false;
+		int result = 1;
+		int totalScore = 0;
+		try {
+			PreparedStatement statement = this.connection.prepareStatement("SELECT USERNAME FROM SCORE;");
+			ResultSet resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				String username = resultSet.getString("USERNAME");
+				
+				if (username.equals(playerName)) {
+
+					String selectSql = "SELECT total_score FROM SCORE WHERE " + "USERNAME = '"+username+"';";
+					System.out.println(selectSql);
+					PreparedStatement selectStatement = this.connection.prepareStatement(selectSql);
+					ResultSet rs = selectStatement.executeQuery();
+
+					while (rs.next()) {
+						int s=rs.getInt("total_score");
+						totalScore =s + (score);
+
+						StringBuffer updateString = new StringBuffer();
+						updateString.append("UPDATE SCORE SET total_score = " + totalScore + " WHERE USERNAME = '"
+								+ username + "';");
+						String updateSql = updateString.toString();
+
+						PreparedStatement updateStatement = connection.prepareStatement(updateSql);
+						Statement stmt = connection.createStatement();
+						stmt.executeUpdate(updateSql);
+						System.out.println("Update successfully");
+						result = result - 1;					
+					}
+					check = true;
+					break;
+
+				}
+			}
+
+			if (check == false) {
+
+				PreparedStatement insertStatement = connection
+						.prepareStatement("INSERT INTO SCORE (USERNAME,TOTAL_SCORE)" + "VALUES (?,?)");
+
+				insertStatement.setString(1, playerName);
+				insertStatement.setInt(2, score);
+				insertStatement.executeUpdate();
+				result = result - 1;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+//public static void main(String[] args) throws IOException {
+//	Database d = new Database();
+//
+//	try (Connection connection = DriverManager.getConnection(d.url, d.username, d.password);
+//			PreparedStatement insertStatement = connection
+//					.prepareStatement("INSERT INTO USERS (Username,Password) " + "VALUES (?,?) ");
+//			PreparedStatement selectStatement = connection
+//					.prepareStatement("SELECT Username, Password FROM USERS WHERE " + "Username= ?")) {
+//
+//		System.out.println("Connection established");
+//
+//		insertStatement.setString(1, "Tom2");
+//		insertStatement.setString(2, "Password");
+//
+//		// execute the statement
+//		insertStatement.executeUpdate();
+//
+//		// more such data
+//		insertStatement.setString(1, "Jim2");
+//		insertStatement.setString(2, "Pass");
+//
+//		insertStatement.executeUpdate();
+//
+//		selectStatement.setString(1, "John");
+//		try (ResultSet resultSet = selectStatement.executeQuery()) {
+//			while (resultSet.next()) {
+//				String username = resultSet.getString("Username");
+//				String password = resultSet.getString("Password");
+//
+//				System.out.println(username + " " + password);
+//			}
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//		// System.out.println("SQLException");
+//	}
 }
+
